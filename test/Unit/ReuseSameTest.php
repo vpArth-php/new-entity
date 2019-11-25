1cb3d8a0abb1611d090416566cf953eca4193b08
Alexander Deider <arth.inbox@gmail.com>
2019-11-26 00:49:10 +0700

    [test] Add stateful behavior clarifying tests

--- test/Unit/ReuseSameTest.php
+++ test/Unit/ReuseSameTest.php
@@ -6,6 +6,7 @@ use Arth\Util\Doctrine\EntityInstantiator;
 use Arth\Util\Doctrine\Identify\FieldSetStrategy;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Test\Unit\Entity\Library\Author;
+use Test\Unit\Entity\Library\Book;
 
 class ReuseSameTest extends DbBase
 {
@@ -44,5 +45,26 @@ class ReuseSameTest extends DbBase
     $author = $this->em->find(Author::class, $entity->id);
     static::assertEquals('Пушкин', $author->title);
     static::assertCount(3, $author->books);
+  }
+
+  public function testDirtyState(): void
+  {
+    $this->svc->setIdentifyStrategy(new FieldSetStrategy(['title']));
+    $book1 = $this->svc->get(Book::class, ['title' => 'One', 'description' => '1']);
+    $book2 = $this->svc->get(Book::class, ['title' => 'One', 'description' => '2']);
+
+    static::assertEquals($book1, $book2);
+    static::assertEquals('2', $book1->description); // First book was updated with latest data
+  }
+  public function testClearState(): void
+  {
+    $this->svc->setIdentifyStrategy(new FieldSetStrategy(['title']));
+    $book1 = $this->svc->get(Book::class, ['title' => 'One', 'description' => '1']);
+    $this->svc->clearState();
+    $book2 = $this->svc->get(Book::class, ['title' => 'One', 'description' => '2']);
+
+    static::assertNotEquals($book1, $book2);
+    static::assertEquals('1', $book1->description);
+    static::assertEquals('2', $book2->description);
   }
 }
